3cd5106ad003d47e5902254617feff091abcbddf
Zin-Mg-Nyunt <zinmgnyunt99@gmail.com>
2026-03-30 19:11:07 +0700

    first state: installation

---
+++ resources/js/app.ts
@@ -0,0 +1,39 @@
+import { createInertiaApp } from '@inertiajs/vue3';
+import { initializeTheme } from '@/composables/useAppearance';
+import AppLayout from '@/layouts/AppLayout.vue';
+import AuthLayout from '@/layouts/AuthLayout.vue';
+import SettingsLayout from '@/layouts/settings/Layout.vue';
+import { ZiggyVue } from 'ziggy-js';
+import { Ziggy } from './ziggy';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => (title ? `${title} - ${appName}` : appName),
+    layout: (name) => {
+        switch (true) {
+            case name === 'Welcome':
+                return null;
+            case name.startsWith('auth/'):
+                return AuthLayout;
+            case name.startsWith('settings/'):
+                return [AppLayout, SettingsLayout];
+            default:
+                return AppLayout;
+        }
+    },
+    progress: {
+        color: '#4B5563',
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({
+            render: () => h(App, props),
+        })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy) // <- ဒါရေး
+            .mount(el);
+    },
+});
+
+// This will set light / dark mode on page load...
+initializeTheme();
